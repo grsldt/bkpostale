965824890c95055df033226d578e6b69cf415e26
grsldt <gregsordel@icloud.com>
2026-03-12 17:21:42 +0100

    vercel

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,26 @@
-import { useEffect } from "react";
+// import { useEffect } from "react";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import axios from "axios";
+// import axios from "axios";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import { Toaster } from "./components/ui/toaster";
 
-const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
-const API = `${BACKEND_URL}/api`;
+// const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
+// const API = `${BACKEND_URL}/api`;
 
 function App() {
-  useEffect(() => {
-    const helloWorldApi = async () => {
-      try {
-        const response = await axios.get(`${API}/`);
-        console.log(response.data.message);
-      } catch (e) {
-        console.error(e, `errored out requesting / api`);
-      }
-    };
-    helloWorldApi();
-  }, []);
+  // useEffect(() => {
+  //   const helloWorldApi = async () => {
+  //     try {
+  //       const response = await axios.get(`${API}/`);
+  //       console.log(response.data.message);
+  //     } catch (e) {
+  //       console.error(e, `errored out requesting / api`);
+  //     }
+  //   };
+  //   helloWorldApi();
+  // }, []);
 
   return (
     <div className="App">
